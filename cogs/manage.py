2fd5e58de954a1ccc0215a162de1bc8889aaf614
Vanilla Black <vanilla@f66.dev>
2023-08-02 00:49:02 -0400

    Refactor Manage cog

--- cogs/manage.py
+++ cogs/manage.py
@@ -15,36 +15,48 @@ class Manage(commands.Cog):
     @manage.command()
     @commands.is_owner()
     async def load(self,
-                     ctx: discord.ApplicationContext,
-                     cog: discord.Option(str, name='cog', description='The cog to load', required=False) = None):
-          if cog:
-            if cog == 'manage':
-                await ctx.respond('You cannot load the manage cog', ephemeral=True)
-                return
-            logger.info(f'Loading cog {cog}')
+            ctx: discord.ApplicationContext,
+            cog: discord.Option(str, name='cog', description='The cog to load', required=True) = None):
+        cog = cog.lower()
+        if cog == 'manage':
+            await ctx.respond('You cannot load the manage cog', ephemeral=True)
+            return
+        logger.info(f'Loading cog {cog}')
+        try:
             bot.load_extension(f'cogs.{cog}')
-            await ctx.respond(f'Loaded cog `{cog}`', ephemeral=True)
+        except Exception as e:
+            logger.error(f'Could not load cog {cog}')
+            logger.error(e)
+            await ctx.respond(f'Cog `{cog}` could not be loaded', ephemeral=True)
+            return
+        await ctx.respond(f'Loaded cog `{cog}`', ephemeral=True)
 
     @manage.command()
     @commands.is_owner()
     async def unload(self,
-                        ctx: discord.ApplicationContext,
-                        cog: discord.Option(str, name='cog', description='The cog to unload', required=False) = None):
-            if cog:
-                if cog == 'manage':
-                    await ctx.respond('You cannot unload the manage cog', ephemeral=True)
-                    return
-                logger.info(f'Unloading cog {cog}')
-                bot.unload_extension(f'cogs.{cog}')
-                await ctx.respond(f'Unloaded cog `{cog}`', ephemeral=True)
+            ctx: discord.ApplicationContext,
+            cog: discord.Option(str, name='cog', description='The cog to unload', required=True) = None):
+        cog = cog.lower()
+        if cog == 'manage':
+            await ctx.respond('You cannot unload the manage cog', ephemeral=True)
+            return
+        if cog not in [x.lower() for x in bot.cogs]:
+            await ctx.respond(f'Cog `{cog}` is not loaded', ephemeral=True)
+            return
+        logger.info(f'Unloading cog {cog}')
+        bot.unload_extension(f'cogs.{cog}')
+        await ctx.respond(f'Unloaded cog `{cog}`', ephemeral=True)
 
     @manage.command()
     @commands.has_permissions(manage_messages=True)
     async def reload(self,
-                     ctx: discord.ApplicationContext,
-                     cog: discord.Option(str, name='cog', description='The cog to reload', required=False) = None):
+            ctx: discord.ApplicationContext,
+            cog: discord.Option(str, name='cog', description='The cog to reload', required=False) = None):
         if cog:
             logger.info(f'Reloading cog {cog}')
+            if cog not in [x.lower() for x in bot.cogs]:
+                await ctx.respond(f'Cog `{cog}` is not loaded', ephemeral=True)
+                return
             bot.reload_extension(f'cogs.{cog}')
             await ctx.respond(f'Reloaded cog `{cog}`', ephemeral=True)
         else:
@@ -53,6 +65,12 @@ class Manage(commands.Cog):
             for cog in cog_names:
                 bot.reload_extension(f'cogs.{cog}')
             await ctx.respond('Reloaded all cogs', ephemeral=True)
+
+    @manage.command()
+    @commands.has_permissions(manage_messages=True)
+    async def loaded(self, ctx: discord.ApplicationContext):
+        cog_names = [x.lower() for x in bot.cogs]
+        await ctx.respond(f'Loaded cogs: `{"`, `".join(cog_names)}`', ephemeral=True)
 
 
 def setup(bot: commands.Bot):
